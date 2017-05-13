f654516ec16475575a04c1e3d63ecbf94e71f3e2
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-13 15:28:52 +0700

    xong server

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
 const express = require('express');
+const session = require('express-session');
+const jsonParser = require('body-parser').json();
 
 const app = express();
+app.use(session({ 
+    secret: 'keyboard cat', 
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: true
+}));
 app.listen(3000, () => console.log('Server started'));
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-
+app.get('/check', (req, res) => {
+    if (req.session.daDangNhap) return res.send('DA_DANG_NHAP');
+    res.send('CHUA_DANG_NHAP');
+});
 
 app.get('*', (req, res) => res.render('home'));
+
+app.post('/dangnhap', jsonParser, (req, res) => {
+    const { username, password } = req.body;
+    if (username === 'pho' && password === '123') {
+        req.session.daDangNhap = true; // eslint-disable-line
+        return res.send('DANG_NHAP_THANH_CONG');
+    }
+    res.send('DANG_NHAP_THAT_BAI');
+});
+
+
