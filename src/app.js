9b4b1625daacaa8ce2da4b6a84ba401d5cf97722
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-13 13:40:14 +0700

    init

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+    <div>
+        <h2>App component</h2>
+    </div>
+);
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')// eslint-disable-line
+);
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: [
+        'script-loader!jquery/dist/jquery.min.js',
+        'script-loader!foundation-sites/dist/js/foundation.min.js',
+        './src/app.js'
+    ],
+    output: {
+        filename: './public/bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                test: /\.jsx?$/,
+                exclude: /node_modules/
+            }
+        ]
+    },
+    devtool: 'cheap-source-map'
+};
