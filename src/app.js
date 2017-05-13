fe33e12bb4394f7a3f32e1f7ddb5cc189604c35c
Van Pho Nguyen <vanpho02@gmail.com>
2017-05-13 14:06:59 +0700

    routers

--- src/app.js
+++ src/app.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { HashRouter, Route, Link } from 'react-router-dom';
+
+const Home = () => <h2>Home component</h2>;
+const Contact = () => <h2>Contact component</h2>;
+const Product = () => <h2>Product component</h2>;
 
 const App = () => (
-    <div>
-        <h2>App component</h2>
-    </div>
+    <HashRouter>
+        <div>
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
+                <li><Link to="/product">Product</Link></li>
+            </ul>
+            <Route exact path="/" component={Home} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/product" component={Product} />
+        </div>
+    </HashRouter>
 );
 
 ReactDOM.render(
     <App />,
     document.getElementById('root')// eslint-disable-line
 );
+
+//https://reacttraining.com/react-router/
